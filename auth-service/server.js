f5fc87581eb48c717b61abe28ec3bee70aea8e24
SanthoshiNaar <santoshi@naarsoft.com>
2025-09-16 19:00:16 +0530

    change port

--- auth-service/server.js
+++ auth-service/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4001;
 app.listen(PORT, () => {
   console.log(`Auth Service running on port ${PORT}`);
 });
