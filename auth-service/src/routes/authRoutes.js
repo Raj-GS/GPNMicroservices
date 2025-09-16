b485c8f307e769a0fee4405a427602ca7b88dd99
SanthoshiNaar <santoshi@naarsoft.com>
2025-09-16 16:40:16 +0530

    docker files are adde2d

--- auth-service/src/routes/authRoutes.js
+++ auth-service/src/routes/authRoutes.js
@@ -13,6 +13,8 @@ router.post('/forgotpassword', authController.forgotPassword);
 router.post('/activateaccountrequest', authController.activateAccountRequest);
 router.post('/checkappuserexistornot', authController.checkAppUserExistOrNot);
 router.post('/deleteuser', authController.deleteDbUser);
-
+router.post('/test', (req, res) => {
+    res.json({ message: 'Test route is working!' });
+});
 
 module.exports = router;
